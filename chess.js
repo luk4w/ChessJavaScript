629d7d66c0b72f7a2c726c8a2a15cf78ddb9e78d
LUCAS <luk4w@outlook.com>
2024-07-01 00:04:54 -0300

    generate fen

--- chess.js
+++ chess.js
@@ -847,3 +847,93 @@ function isIllegalMove(bitboards) {
     const OPPONENT_COLOR = selectedColor === WHITE ? BLACK : WHITE;
     return bitboards[selectedColor][KING] & getAllMoves(bitboards, OPPONENT_COLOR);
 }
+
+/**
+    @FEN (Forsyth-Edwards Notation)
+    Notação usada para descrever o estado de um tabuleiro de xadrez.
+
+    @EXEMPLO_ESTADO_INICIAL
+    rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
+
+    @POSICOES_DO_TABULEIRO
+    Os campos separados por / representam as fileiras do tabuleiro, cada letra representa uma peça:
+    r - black rook ; n - black knight ; b - black bishop ; q - black queen ; k - black king ; p - black pawn
+    R - white rook ; N - white knight ; B - white bishop ; Q - white queen ; K - white king ; P - white pawn
+    Os números representam as casas vazias.
+
+    RNBQKBNR
+    PPPPPPPP
+    8
+    8
+    8
+    8
+    pppppppp
+    rnbqkbnr
+
+    @TURNO_DE_QUEM_JOGA
+    w = white; b = black.
+
+    @ROQUE
+    K - white kingside castling ; Q - white queenside castling
+    k - black kingside castling ; q - black queenside castling
+    - - sem possibilidade de roque
+
+    @CAPTURA_EN_PASSANT
+    Caso exista a possibilidade de captura en passant, a posição do peão adversário é informada.
+
+    @MEIO_MOVIMENTO
+    O número de meios movimentos desde a última captura ou movimento de peão.
+
+    @NUMERO_DE_JOGADAS
+    O número de jogadas completas (brancas jogam e pretas jogam).
+*/
+
+function generateFEN() {
+    const PIECES = ["p", "n", "b", "r", "q", "k"];
+    let fen = '';
+    let emptyCount = 0;
+
+    // itera sobre as 8 linhas do tabuleiro
+    for (let x = 7; x >= 0; x--) {
+        // itera sobre as 8 colunas do tabuleiro
+        for (let y = 7; y >= 0; y--) {
+            let index = x * 8 + y;
+            let piece = null;
+            for (let i = 0; i < 6; i++) {
+                // Verifica se existem peças (brancas ou pretas) na posição indicada
+                if (bitboards[WHITE][i] & (1n << BigInt(index))) {
+                    // Converte a peça para a notação FEN
+                    piece = PIECES[i].toUpperCase();
+                    break;
+                } else if (bitboards[BLACK][i] & (1n << BigInt(index))) {
+                    // Converte a peça para a notação FEN
+                    piece = PIECES[i];
+                    break;
+                }
+            }
+            // Piece !== null, significa que existe uma peça na posição
+            if (piece !== null) { // if(piece) {...} também funciona
+                // Se existem casas vazias antes da peça
+                if (emptyCount > 0) {
+                    fen += emptyCount; // Adiciona o número de casas vazias ao FEN
+                    emptyCount = 0; // Reseta o contador de casas vazias
+                }
+                fen += piece; // Adiciona a peça ao FEN
+            } else {
+                emptyCount++; // Incrementa o contador de casas vazias
+            }
+        }
+        // Se existem casas vazias no final da linha
+        if (emptyCount > 0) {
+            // Adiciona o número de casas vazias ao FEN
+            fen += emptyCount;
+            emptyCount = 0; // Reseta o contador de casas vazias
+        }
+        // Se não for a última linha
+        if (x > 0) {
+            // Adiciona a barra para separar as linhas
+            fen += '/';
+        }
+    }
+    return fen;
+}
